216e35f0c385a257777a291faeded0311279706e
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-08-29 16:53:33 -0500

    styles landing

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import background from '../../assets/foodlanding.png'
+import background from '../../assets/foodlanding02.png'
 
 
 const Navbar = () => {
 
 
   return (
-    <header className="bg-white fixed z-50 top-0 left-0 w-full shadow-md transition duration-500" >    
+    <header className="bg-white fixed z-50 top-0 left-0 w-full shadow-md transition duration-500" >
 
       <>
         <section className="relative bg-white">
           <img
-            className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full  "
+            className="absolute inset-0 object-[75%]  object-cover w-full h-full  "
             src={background}
             alt="Couple on a bed with a dog"
           />
-          <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent" />
-          <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-            <div className="max-w-xl text-center sm:text-left">
-              <h1 className="font-extrabold sm:text-7xl">
-                <strong className="text-7xl font-extrabold text-teal-900 sm:block">
-                  
-                Welcome
-                </strong>                
-                <strong className="text-5xl font-extrabold text-yellow-900 sm:block py-2">
+          <div className="block inset-0 absolute bg-gradient-to-r from-white via-transparent to-transparent" />
+          <div className="relative max-w-screen-xl px-4 py-32 mx-auto h-screen ">
+            <div className="max-w-xl text-left">
+              <h1 className="flex flex-col justify-start font-extrabold ">
+                <strong className="text-5xl sm:text-7xl md:text-8xl mt-4 sm:leading-relaxed text-teal-900  ">
+                  Welcome
+                </strong>
+                <strong className="text-5xl font-extrabold text-amber-900 sm:block py-2">
                   hungry ?
                 </strong>
               </h1>
               <div className="flex flex-wrap gap-4 mt-8 text-center">
-                <button type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-full text-md px-10 py-2.5 text-center mr-2 mb-2">
+                <button type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-600 to-teal-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-orange-800/70 font-medium rounded-full text-md px-10 py-2.5 text-center mr-2 mb-2">
                   go
                 </button>
 
