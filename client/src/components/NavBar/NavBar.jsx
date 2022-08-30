1be4bd4a1db4bad202be53d37004cbfa42f229aa
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-08-30 08:43:48 -0500

    Update landing  styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -8,51 +8,32 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="bg-white fixed z-50 top-0 left-0 w-full shadow-md transition duration-500" >
-
-      <>
-        <section className="relative bg-white">
-          <img
-            className="absolute inset-0 object-[75%]  object-cover w-full h-full  "
-            src={background}
-            alt="Couple on a bed with a dog"
-          />
-          <div className="block inset-0 absolute bg-gradient-to-r from-white via-transparent to-transparent" />
-          <div className="relative max-w-screen-xl px-4 py-32 mx-auto h-screen ">
-            <div className="max-w-xl text-left">
-              <h1 className="flex flex-col justify-start font-extrabold ">
-                <strong className="text-6xl md:text-8xl lg:text-9xl transition ease-in-out delay-150  text-teal-900  ">
-                  Welcome
-                </strong>
-                <strong className="text-5xl font-extrabold text-amber-900 sm:block py-2">
-                  hungry ?
-                </strong>
-              </h1>
-              <div className="flex flex-wrap gap-4 mt-8 text-center">
-                <button onClick={()=>navigate('/home')} type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-600 to-teal-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-orange-800/70 font-medium rounded-full text-md px-10 py-2.5 text-center mr-2 mb-2">
-                  go
-                </button>
-
-              </div>
-              <div className="flex flex-wrap gap-4 mt-8 text-center"></div>
-              {/* <div className="flex flex-wrap gap-4 mt-8 text-center">
-              {isAuthenticated ? (
-                <>
-                  <Logout />
-                </>
-              ) : (
-                <Login />
-              )}
-            </div> */}
+    
+    
+    <section className="relative bg-white">
+        <img
+          className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-40 sm:opacity-100"
+          src={`${background}?auto=compress&cs=tinysrgb&h=627&fit=crop&w=1200`}
+          alt="Couple on a bed with a dog"
+        />
+        <div className="hidden sm:block sm:inset-0 sm:absolute bg-gradient-to-r from-white via-transparent to-transparent " />
+        <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
+          <div className="max-w-xl text-center sm:text-left">
+            <h1 className="text-6xl font-extrabold text-teal-900 font-comforta-300 sm:text-7xl">
+              Welcome
+              <br />
+              <strong className="text-4xl font-extrabold text-amber-900 sm:block  sm:py-4">
+                Hungry ?
+              </strong>
+            </h1>
+            <div className="flex flex-wrap gap-4 mt-8 text-center"></div>
+            <div className="max-w-lg sm:leading-relaxed sm:text-xl">
+               <button onClick={() => navigate('/home')} type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-600 to-teal-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-orange-800/70 font-medium rounded-full text-md px-12 py-1.5 text-center mr-2 mb-2">go</button>
             </div>
           </div>
-        </section>
-      </>
+        </div>
+      </section>
 
-
-
-
-    </header>
   )
 }
 
