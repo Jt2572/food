661fd3ce384038669b7191dee699e491aa810f04
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-10-28 20:39:36 -0500

    Update recipesActions.js

--- client/src/features/reducers/recipesActions.js
+++ client/src/features/reducers/recipesActions.js
@@ -3,24 +3,24 @@ import { getAllRecipes, searchRecipes, getDiets, searchRecipeById } from "../red
 
 
 export const getRecipes = () => (dispatch) => {
-    axios("https://af-w0hf.onrender.com/recipes")
-    // axios("https://api-food2.herokuapp.com/recipes")
+    // axios("https://af-w0hf.onrender.com/recipes")
+    axios("https://api-food2.herokuapp.com/recipes")
     // axios("http://localhost:3001/recipes")
         .then(res => { dispatch(getAllRecipes(res.data)) })
         .catch(e => console.log(e))
 }
 
 export const searchRecipeName = (recipe) => (dispatch) => {
-    axios(`https://af-w0hf.onrender.com/recipes?name=${recipe}`)
-    // axios(`https://api-food2.herokuapp.com/recipes?name=${recipe}`)
+    // axios(`https://af-w0hf.onrender.com/recipes?name=${recipe}`)
+    axios(`https://api-food2.herokuapp.com/recipes?name=${recipe}`)
     // axios(`http://localhost:3001/recipes?name=${recipe}`)
         .then(res => { console.log(res); dispatch(searchRecipes(res.data)) })
         .catch(e => console.log(e))
 }
 
 export const searchRecipeId = (id) => (dispatch) => {
-    axios(`https://af-w0hf.onrender.com/recipes/${id}`)
-    // axios(`https://api-food2.herokuapp.com/recipes/${id}`)
+    // axios(`https://af-w0hf.onrender.com/recipes/${id}`)
+    axios(`https://api-food2.herokuapp.com/recipes/${id}`)
     // axios(`http://localhost:3001/recipes/${id}`)
         .then(res => { console.log(res.data); dispatch(searchRecipeById([res.data])) })
         .catch(e => console.log(e))
@@ -29,6 +29,7 @@ export const searchRecipeId = (id) => (dispatch) => {
 
 export const getAllDiets = () => (dispatch) => {
     axios("https://api-food2.herokuapp.com/types")
+    // axios("https://af-w0hf.onrender.com/types")
     // axios("http://localhost:3001/types")
         .then(res => { dispatch(getDiets(res.data)) })
         .catch(e => console.log(e))
@@ -38,8 +39,9 @@ export const createRec = (payload) => {
     return async () => {
         try {
             await axios.post(
-                "https://af-w0hf.onrender.com/createrecipe",
-                // "https://api-food2.herokuapp.com/createrecipe",
+                // "https://af-w0hf.onrender.com/createrecipe",
+                "https://api-food2.herokuapp.com/createrecipe",
+                // "http://localhost:3001/createrecipe",
                 payload
             );
         } catch (error) {
