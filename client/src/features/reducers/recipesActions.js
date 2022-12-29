6a884e339001e0de1489e92a108161b90124d509
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-12-29 12:32:36 -0500

    railway endpoints

--- client/src/features/reducers/recipesActions.js
+++ client/src/features/reducers/recipesActions.js
@@ -4,7 +4,8 @@ import { getAllRecipes, searchRecipes, getDiets, searchRecipeById } from "../red
 
 export const getRecipes = () => (dispatch) => {
     // axios("https://af-w0hf.onrender.com/recipes")
-    axios("https://api-food2.herokuapp.com/recipes")
+    // axios("https://api-food2.herokuapp.com/recipes")
+    axios("https://apifood-production-4fdf.up.railway.app/recipes")
     // axios("http://localhost:3001/recipes")
         .then(res => { dispatch(getAllRecipes(res.data)) })
         .catch(e => console.log(e))
@@ -12,7 +13,8 @@ export const getRecipes = () => (dispatch) => {
 
 export const searchRecipeName = (recipe) => (dispatch) => {
     // axios(`https://af-w0hf.onrender.com/recipes?name=${recipe}`)
-    axios(`https://api-food2.herokuapp.com/recipes?name=${recipe}`)
+    // axios(`https://api-food2.herokuapp.com/recipes?name=${recipe}`)
+    axios(`https://apifood-production-4fdf.up.railway.app/recipes?name=${recipe}`)
     // axios(`http://localhost:3001/recipes?name=${recipe}`)
         .then(res => { console.log(res); dispatch(searchRecipes(res.data)) })
         .catch(e => console.log(e))
@@ -20,7 +22,8 @@ export const searchRecipeName = (recipe) => (dispatch) => {
 
 export const searchRecipeId = (id) => (dispatch) => {
     // axios(`https://af-w0hf.onrender.com/recipes/${id}`)
-    axios(`https://api-food2.herokuapp.com/recipes/${id}`)
+    // axios(`https://api-food2.herokuapp.com/recipes/${id}`)
+    axios(`https://apifood-production-4fdf.up.railway.app/recipes/${id}`)
     // axios(`http://localhost:3001/recipes/${id}`)
         .then(res => { console.log(res.data); dispatch(searchRecipeById([res.data])) })
         .catch(e => console.log(e))
@@ -28,7 +31,8 @@ export const searchRecipeId = (id) => (dispatch) => {
 
 
 export const getAllDiets = () => (dispatch) => {
-    axios("https://api-food2.herokuapp.com/types")
+    axios("https://apifood-production-4fdf.up.railway.app/types")
+    // axios("https://api-food2.herokuapp.com/types")
     // axios("https://af-w0hf.onrender.com/types")
     // axios("http://localhost:3001/types")
         .then(res => { dispatch(getDiets(res.data)) })
@@ -40,7 +44,8 @@ export const createRec = (payload) => {
         try {
             await axios.post(
                 // "https://af-w0hf.onrender.com/createrecipe",
-                "https://api-food2.herokuapp.com/createrecipe",
+                // "https://api-food2.herokuapp.com/createrecipe",
+                "https://apifood-production-4fdf.up.railway.app/createrecipe",
                 // "http://localhost:3001/createrecipe",
                 payload
             );
